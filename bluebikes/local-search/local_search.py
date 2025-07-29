73da55632defbd9375867ac0ed41a3780f45fa7a
Ethan Orlowsky <ethaniorlowsky@gmail.com>
2025-07-28 23:10:08 -0400

    experiment with a couple different local optimizations

--- bluebikes/local-search/local_search.py
+++ bluebikes/local-search/local_search.py
@@ -3,8 +3,9 @@ from bluebikes.tsp.helpers import load_route, load_stations, load_distance_matri
 from bluebikes.tsp.get_directions import main as create_map_w_directions
 from python_tsp.heuristics import solve_tsp_local_search
 
-ROUTE_FILE = "outputs/routes/shortest_distance_route.csv"
-OUTPUT_FILE = ROUTE_FILE.replace(".", "_local_search.")
+ROUTE_FILE = "outputs/routes/shortest_distance_route_ps2.csv"
+PETURBATION_SCHEME = "ps6"
+OUTPUT_FILE = ROUTE_FILE.replace(".", f"_{PETURBATION_SCHEME}.")
 max_stations = None
 
 stations = load_stations()
@@ -15,7 +16,7 @@ selected_matrix = distance_matrix[:max_stations, :max_stations] if max_stations 
 last_route = load_route(ROUTE_FILE) if os.path.exists(ROUTE_FILE) else None
 x0 = [waypoint["station_id"] for waypoint in last_route] if last_route and len(last_route) == len(selected_stations) else None
 
-raw_route, _  = solve_tsp_local_search(distance_matrix, x0, perturbation_scheme="ps2")
+raw_route, _  = solve_tsp_local_search(distance_matrix, x0, perturbation_scheme=PETURBATION_SCHEME)
 route = format_python_tsp_route(selected_stations, raw_route)
 write_route_to_csv(route, OUTPUT_FILE)
 create_map_w_directions(OUTPUT_FILE)
